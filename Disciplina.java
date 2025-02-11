83a0d23670a644cd4a5039f938f5373197367da4
SamCortland <148400402+SamCortland@users.noreply.github.com>
2025-02-11 01:14:12 +0000

    aula1

---
+++ Disciplina.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Disciplina{
+    private String nomeDisciplina;
+    private List<Aluno> alunos;
+    private List<Professor> professores;
+
+    public Disciplina(){
+        this("", new ArrayList<Aluno>(), new ArrayList<Professor>());
+    }
+
+    public Disciplina(String nomeDisciplina){
+        this(nomeDisciplina, new ArrayList<Aluno>() , new ArrayList<Professor>());
+    }
+
+    public Disciplina(String nomeDisciplina, List<Aluno> alunos, List<Professor> professores){
+        this.nomeDisciplina = nomeDisciplina;
+        this.alunos = alunos;
+        this.professores = professores;
+    }
+
+    public void serNomeDisciplina(String nomeDisciplina){
+        this.nomeDisciplina = nomeDisciplina;
+    }
+
+    public String getNomeDisciplina(){
+        return this.nomeDisciplina;
+    }
+
+    public void setAlunos(List<Aluno> alunos){
+        this.alunos = alunos;
+    }
+
+    public List<Aluno> getAlunos(){
+        return this.alunos;
+    }
+
+    public void setProfessores(List<Professor> professores){
+        this.professores = professores;
+    }
+
+    public List<Professor> getProfessores(){
+        return this.professores;
+    }
+
+    public void addAluno(Aluno aluno){
+        this.alunos.add(aluno);
+    }
+
+    public void addProfessor(Professor professor){
+        this.professores.add(professor);
+    }
+
+    @Override
+    public String toString(){
+        return "Disciplina{=" + getNomeDisciplina() + 
+                ", Alunos=" + getAlunos() +
+                ", Professores=" + getProfessores();
+    }
+
+}
---
+++ Principal.java
@@ -0,0 +1,11 @@
+public class Principal{
+
+    public static void main(String args[]){
+
+    Aluno aluno1 = new Aluno("Kaueh", 21, 1256789689646101l , 1001);
+    System.out.println(aluno1.toString());
+
+    Professor professor1 = new Professor("Cleber", 54, 12345678999l, 10002);
+    System.out.println(professor1.toString());
+    }
+}
